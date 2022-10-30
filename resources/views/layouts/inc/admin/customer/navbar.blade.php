61a9ce72e565cb9504da663e847da2296f9f9fa0
Taha Çiçek <ttahacicek@gmail.com>
2022-10-30 15:30:14 +0300

    navbar fixed.

--- resources/views/layouts/inc/admin/customer/navbar.blade.php
+++ resources/views/layouts/inc/admin/customer/navbar.blade.php
@@ -46,54 +46,55 @@
                         <a href="#" class="nav-menu-item"><i class="fa fa-search me-3"></i>Trendler</a>
                         <a href="#" class="nav-menu-item"><i class="fa fa-wrench me-3"></i>Elektronik</a>
                         <a href="#" class="nav-menu-item"><i class="fa fa-dollar me-3"></i>Moda</a>
+                        @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="blackLink text-white nav-menu-item"
+                                    href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="blackLink text-white nav-menu-item"
+                                    href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown ">
+                            <a class="blackLink siteLink text-white nav-menu-item dropdown-toggle"
+                                style="border-right:1px solid #eaeaea" class="" href="#" id="navbarDropdown"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa fa-user"></i> {{ Auth::user()->name }}
+
+                            </a>
+                            <ul class="dropdown-menu drop dropdown-content" aria-labelledby="navbarDropdown">
+                                <li><a class=" btn-sm dropdown-item  dropbtn" href="#"><i class="fa iconss fa-user"></i> <small>Profil</small></a></li>
+                                <li><a class="dropdown-item whiteLink siteLink dropbtn" href="#"><i
+                                            class="fa iconss fa-list"></i> My Orders</a>
+                                </li>
+                                <li><a class=" dropdown-item dropbtn" href="#"><i class="fa iconss fa-heart"></i>  My Wishlist</a>
+                                </li>
+                                <li><a class=" dropdown-item dropbtn" href="#"><i class="fa iconss fa-shopping-cart"></i> My
+                                        Cart</a></li>
+                                <li>
+                                    <a class="dropdown-item dropbtn" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+                                        <i class="fa iconss fa-sign-out"></i>
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endguest
                         <a href="#" class="nav-menu-item"><i class="fa fa-file me-3"></i>Aksesuar</a>
                         {{-- <a href="#" class="nav-menu-item"><i class="fa fa-building me-3"></i>About Us</a> --}}
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="blackLink text-white nav-menu-item"
-                                        href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="blackLink text-white nav-menu-item"
-                                        href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a class="blackLink siteLink text-white nav-menu-item dropdown-toggle"
-                                    style="border-right:1px solid #eaeaea" class="" href="#" id="navbarDropdown"
-                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-user"></i> {{ Auth::user()->name }}
-
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item " href="#"><i class="fa fa-user"></i> Profile</a></li>
-                                    <li><a class="dropdown-item whiteLink siteLink" href="#"><i
-                                                class="fa fa-list"></i> My Orders</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="#"><i class="fa fa-heart"></i> My Wishlist</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="#"><i class="fa fa-shopping-cart"></i> My
-                                            Cart</a></li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-              document.getElementById('logout-form').submit();">
-                                            <i class="fa fa-sign-out"></i>
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
                     </ul>
                 </div>
             </nav>
